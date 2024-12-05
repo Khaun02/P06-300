d42674b61064bfe9790d507563940cda21284b28
Khaun02 <khaunx02@gmail.com>
2024-12-05 15:36:23 -0600

    0.2
    Finalized BSTNode.java
    
    - Completely implemented all of the methods for the BSTNode.java
    x - Has not tested

--- BSTNode.java
+++ BSTNode.java
@@ -4,32 +4,39 @@
  * 
  * @param <T> any data type that you want the node to hold
  */
-
+public class BSTNode<T> {
+    private T data;
+    private BSTNode<T> left;
+    private BSTNode<T> right;
+    // The constructor
     public BSTNode(T data) {
-        
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 
     public T getData() {
-
+        return this.data;
     }
 
     public void setData(T data) {
-
+        this.data = data;
     }
 
     public BSTNode<T> getLeft() {
-
+        return this.left;
     }
 
     public void setLeft(BSTNode<T> left) {
-
+        this.left = left;
     }
 
     public BSTNode<T> getRight() {
-
+        return this.right;
     }
 
     public void setRight(BSTNode<T> right) {
-
+        this.right = right;
     }
 }
+    
