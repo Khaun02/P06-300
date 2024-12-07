e9bf5a8802c181486270b4651187cd690ea6a21e
Khaun02 <khaunx02@gmail.com>
2024-12-07 17:52:12 -0600

    0.4
    Completed addMovie method and size method
    
    - Completed both of those methods for the MovieTree.java file
    - Also made sure they work in my testers

--- MovieTree.java
+++ MovieTree.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /** Generic class for a node
  * 
  * @author Shaun Khang
@@ -5,12 +7,101 @@
  */
 public class MovieTree {
     
+    private BSTNode<Movie> root;
+    
+    // the addmovie and addmovie helper
+    // we use a helper method so its easier to use recursion
+
     public boolean addMovie(Movie movie) {
+        
+        if (root == null) {
+            root = new BSTNode<Movie>(movie);
+            return true;
+        }
+
+        return addMovieHelper(root, movie);
+
+    }
+    
+    /*
+     * Easier to use recursion if you have a helper method so thats why we use it
+     * The current represents the node that the BST is checking during the RECURSIVE process...
+     * 
+     *  so after you go with the root node, then it either moves left or right based on the 
+     *  movie being added
+     */
+    private boolean addMovieHelper(BSTNode<Movie> current, Movie movie) {
+        
+        // goes left
+        if (movie.compareTo(current.getData()) < 0) {
+            if (current.getLeft() == null) {
+                //set the current node we're on child to be right
+                current.setLeft(new BSTNode<Movie>(movie));
+                return true;
+            }
+            //else we keep recursively going down
+            else {
+                return addMovieHelper(current.getLeft(), movie);
+            }
+        }
+        // goes right
+        else if (movie.compareTo(current.getData()) > 0) {
+            if (current.getRight() == null) {
+                //set the current node we're on child to be right
+                current.setRight(new BSTNode<Movie>(movie));
+                return true;
+            }
+            //else we keep recursively going down
+            else {
+                return addMovieHelper(current.getRight(), movie);
+            }
+        }
+        // if the movie is a duplicate, will return false and not add anything
+        else if (movie.compareTo(current.getData()) == 0) {
+            return false;
+        }
+
+        return false;
+    }
+
+    /*
+     * method to check the size of the tree
+     */
+    public int size() {
+        
+        if (root == null) {
+            return 0;
+        }
+
+        return sizeHelper(root);
 
     }
 
-    public int size() {
+    /*
+     * uses recursion to help the size method easier
+     * 
+     * the size of a tree is:
+     * size of left subtree + right subtree + 1(the node)
+     * 
+     */
+    private int sizeHelper(BSTNode<Movie> current) {
+        // we already checked in the size method for a null tree, which should return a size of 0
+        // so now we're using recursion to check the rest of the tree
         
+        // base case
+        // states when the recursion should stop, prevents the method from calling itself indefinitely
+        // often the simplest case
+        // so for the sizeHelper, it should stop when the current node is null, which means there is nothing to the right/left
+        if (current == null) {
+            return 0;
+        }
+        // To calculate the size of the tree, you need to traverse both the left and right subtrees of the current node.
+        if (current.getLeft() == null) {
+            return 0;
+            sizeHelper(current.getLeft());
+            current.
+        }
+        return 0;
     }
 
     public int height() {
@@ -29,13 +120,7 @@ public class MovieTree {
 
     }
 
-    private boolean addMovieHelper(BSTNode<Movie> current, Movie movie) {
 
-    }
-
-    private int sizeHelper(BSTNode<Movie> current) {
-
-    }
 
     private int heightHelper(BSTNode<Movie> current) {
 
--- MovieTreeTester.java
+++ MovieTreeTester.java
@@ -5,10 +5,39 @@
  */
 public class MovieTreeTester {
     public static boolean testAddMovie() {
-        return false;
+        MovieTree movieTree = new MovieTree();
+
+        Movie movie1 = new Movie(1994, 9.3, "The Shawshank Redemption");
+        Movie movie2 = new Movie(2008, 9.0, "The Dark Knight");
+        Movie movie3 = new Movie(2010, 8.8, "Inception");
+   
+        if (movieTree.addMovie(movie1) == false) {
+            System.out.println("Test 1 of testAddMovie failed");
+            return false;
+        }
+        if (movieTree.addMovie(movie2) == false) {
+            System.out.println("Test 2 of testAddMovie failed");
+            return false;
+        }
+        if (movieTree.addMovie(movie3) == false) {
+            System.out.println("Test 3 of testAddMovie failed");
+            return false;
+        }
+        // edge case with adding the same movie
+        // edge case with adding something thats not a movie, try catch statements
+        return true;
+        
     }
     public static boolean testSize() {
-        return false;
+        MovieTree movieTree = new MovieTree();
+        
+        // should just go through this method since i havent added anything to the movieTree
+        if (movieTree.size() != 0) {
+            System.out.println("Test 1 for testSize failed.");
+            return false;
+        }
+        
+        return true;
     }
     public static boolean testHeight() {
         return false;
